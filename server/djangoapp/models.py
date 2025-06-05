8d7fb3ea260b81f5255b3b47d61e8fa3b1ad84f3
michl15 <h.michelle.lee@gmail.com>
2025-06-04 20:19:02 -0400

    adding models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -12,6 +12,12 @@
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'CarMake(Name: {name}, Description: {description})'
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -23,3 +29,19 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+TYPE_CHOICES = {
+    'SEDAN': "Sedan",
+    'SUV': "SUV",
+    'WAGON': "Wagon"
+}
+
+class CarModel(models.Model):
+    name = models.CharField(max_length=30)
+    dealerId = models.IntegerField()
+    carType = models.CharField(choices=TYPE_CHOICES)
+    year = models.IntegerField()
+    carMake = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'CarModel(Name: {name}, Dealer: {dealerId}, CarType: {carType}, Year: {year}, CarMake:{carMake})'
